177ad295e854c085f1eca31a33c00d72306a8c66
denis14235 <159535580+denis14235@users.noreply.github.com>
2024-02-09 17:10:36 +0600

    Add files via upload

---
+++ mtsplt.sh
@@ -0,0 +1,8 @@
+gem install bundler
+NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
+gem install nokogiri -v $NOKOGIRI_VERSION -- --with-cflags="-Wno-implicit-function-declaration -Wno-deprecated-declarations -Wno-incompatible-function-pointer-types" --use-system-libraries
+bundle install
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
